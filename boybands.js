9ea1e263cd2115e9265710ceed60c619a151a87f
MyMac <mymac@MyMacs-MacBook-Pro.local>
2017-12-01 15:32:54 -0600

    eleminated pseudocode

--- boybands.js
+++ boybands.js
@@ -29,26 +29,3 @@ looper (bands , vegetables);
 
 
 
-
-/*
-var loopCount = bands.length;
-var currentBand = bands[loopCount];
-var currentVeggie = vegetables[loopCount];
-
-var bandElement = document.getElementById("boy-bands");
-var veggieElement = document.getElementById("vegetables");
-
-
-
-// Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-  
-  // Add the band names into the correct <div>
-  currentBand = document.write(bandElement);
-
-  
-  // Add the veggie names into the correct <div>
-  currentVeggie = document.write(veggieElement);
-
-}
-*/
